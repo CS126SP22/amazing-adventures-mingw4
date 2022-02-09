20edd7d64d282d5bcb3f73a433bf3f91fc3e8bbc
mingw4 <mingw4@illinois.edu>
2022-02-08 23:33:30 -0600

    Java Class created, and layout built with objection decomposition.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,19 @@
+import java.io.IOException;
+import java.net.URL;
+
+import student.adventure.Driver;
+
 public class Main {
-    public static void main(String[] args) {
+    private static String DEFAULT_RESOURCE_NAME = "siebel.json";
+
+    public static void main(String[] args) throws IOException {
         // Wishing you good luck on your Adventure!
+        String name = args.length == 0 ? DEFAULT_RESOURCE_NAME : args[0];
+        URL src = Main.class.getResource(name);
+        if (src == null) {
+            System.out.println("No resource with the name \"" + name + "\" is found.");
+            return;
+        }
+        Driver.run(src);
     }
 }
